114d6211325a1d599f4a181ea2c37bc897ef8219
Minh_Kim_Long <kimlongonlylove@gmail.com>
2020-07-14 12:22:46 +0700

    + Commit create source

---
+++ SwiftShopping/SwiftShopping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftShopping
+//
+//  Created by ITS-Japan on 7/14/20.
+//  Copyright © 2020 ITS-Japan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
